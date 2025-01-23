746caf7ada7613125b5f267afb5c6cad294cfe2a
ahorethcbna <aaron.horeth@cbna.com>
2025-01-23 19:33:21 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,24 +1,68 @@
 // create web server
-// 1. import http module
-// 2. create a server
-// 3. listen to a port
 const http = require('http');
-
-// 2. create a server
-const server = http.createServer((req, res) => {
-    if (req.url === '/comments') {
-        res.write('Comments page');
-        res.end();
-    } else if (req.url === '/posts') {
-        res.write('Posts page');
-        res.end();
-    } else {
-        res.write('Home page');
-        res.end();
+const fs = require('fs');
+const url = require('url');
+const path = require('path');
+const comments = [
+    {
+        name: '张三',
+        message: '今天天气不错',
+        dateTime: '2020-01-01'
+    },
+    {
+        name: '李四',
+        message: '今天天气不错',
+        dateTime: '2020-01-01'
+    },
+    {
+        name: '王五',
+        message: '今天天气不错',
+        dateTime: '2020-01-01'
+    },
+    {
+        name: '赵六',
+        message: '今天天气不错',
+        dateTime: '2020-01-01'
     }
-});
-
-// 3. listen to a port
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+];
+http.createServer((req, res) => {
+    const parseObj = url.parse(req.url, true);
+    const pathname = parseObj.pathname;
+    if (pathname === '/') {
+        fs.readFile(path.join(__dirname, 'views', 'index.html'), 'utf8', (err, data) => {
+            if (err) {
+                return res.end('404 Not Found');
+            }
+            let htmlStr = '';
+            comments.forEach(item => {
+                htmlStr += `
+                    <li>
+                        <p>姓名：${item.name}</p>
+                        <p>评论：${item.message}</p>
+                        <p>时间：${item.dateTime}</p>
+                    </li>
+                `;
+            });
+            data = data.replace('^_^', htmlStr);
+            res.end(data);
+        });
+    } else if (pathname.indexOf('/public/') === 0) {
+        fs.readFile(path.join(__dirname, pathname), (err, data) => {
+            if (err) {
+                return res.end('404 Not Found');
+            }
+            res.end(data);
+        });
+    } else if (pathname === '/post') {
+        fs.readFile(path.join(__dirname, 'views', 'post.html'), 'utf8', (err, data) => {
+            if (err) {
+                return res.end('404 Not Found');
+            }
+            res.end(data);
+        });
+    } else if (pathname === '/pinglun') {
+        const comment = parseObj.query;
+        comment.dateTime = '2020-01-01';
+        comments.unshift(comment);
+        res.statusCode = 302;
+        res
